24d57256fd6e9c2dbe13a22855cef91c6502a51c
Thomas Sharkey <tbsharkey@gmail.com>
2017-10-26 10:26:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(ar)
+  ar.sort
+end
 
+def sort_array_desc(ar)
+ar.sort { |a, b| b <=> a}
+end
+
+def sort_array_char_count(ar)
+  ar.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(ar)
+ first = ar.shift
+ ar.reverse.unshift(first)
+ end
+
+def reverse_array(ar)
+  ar.reverse
+end
+
+def kesha_maker(ar)
+ar.each{|a| a[2]="$"}
+end
+
+def find_a(ar)
+  ar.select {|i| i.start_with?("a")}
+end
+
+def sum_array(ar)
+  ar.inject { |a, b| a + b}
+end
+
+def add_s(ar)
+  ar.each_with_index.collect do |element, index|
+    if index!= 1
+      element + "s"
+    else
+      element
+    end
+    end
+  end
